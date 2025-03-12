9c19f03bfa61c7063dc3f3656ca1d8fbbe6f12d1
bubbletealover12 <willchenworks@gmail.com>
2025-03-12 15:07:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def callback():
 
     session["user_info"] = user_info
 
-    return redirect("http://localhost:3000")
+    return redirect("https://frontend-main.osc-fr1.scalingo.io")
 
 
 # New API route to sync user data (POST)
